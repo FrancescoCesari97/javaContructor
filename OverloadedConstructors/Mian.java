6e49545f5940bbf21aa90f22d5906cdff20e4792
FrancescoCesari97 <freedus97@gmail.com>
2024-06-12 16:48:46 +0200

    Overloaded Constructor

---
+++ OverloadedConstructors/Mian.java
@@ -0,0 +1,20 @@
+public class Mian {
+    public static void main(String[] args) {
+
+        // overloaded constructors = multiple constructors within a class with the same
+        // name,
+        // but have different parameters
+        // name + parameters = signature
+
+        Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella");
+
+        System.out.println("here are the ingredients of your pizza: ");
+
+        System.out.println(pizza.bread);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.topping);
+
+    }
+
+}
